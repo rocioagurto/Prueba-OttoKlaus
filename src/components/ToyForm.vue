4377aecf41550a46a4a9011be1528c94442b050f
Rocio Agurto <rocio.agurto1986@gmail.com>
2020-06-15 16:58:14 -0400

    segundo commit

--- src/components/ToyForm.vue
+++ src/components/ToyForm.vue
@@ -4,6 +4,7 @@
     <v-dialog
       :value="showForm"
       width="500"
+      persistent
       
     >
     <v-card>
@@ -14,21 +15,19 @@
         Privacy Policy
     </v-card-title>
 
-    <v-card-text>
-        
-    </v-card-text>
+
+    <v-text-field label="nombre" type="text" :value="currentToy.data.name" @input="updateName"/>
+    <v-text-field label="precio" prefix="$" :value="currentToy.data.price" @input="updatePrice"/>
+    <v-text-field label="codigo" type="text" :value="currentToy.data.code" @input="updateCode"/>
+    <v-text-field label="stock" suffix="Unidades" :value="currentToy.data.stock" @input="updateStock"/>
+
+    
 
     <v-divider></v-divider>
 
     <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn
-        color="primary"
-        text
-        @click="submitForm"
-        >
-        I accept
-        </v-btn>
+        <v-btn color="primary" text @click="submitForm">I accept</v-btn>
     </v-card-actions>
     </v-card>
     </v-dialog>
@@ -40,13 +39,14 @@
 import {mapState, mapActions} from 'vuex'
 export default {
     methods: {
-        ...mapActions(['hideToyForm']),
+        ...mapActions(['hideToyForm', 'updateName', 'updatePrice', 'updateCode', 'updateStock', 'postToy']),
         submitForm(){
+            this.postToy()
             this.hideToyForm()
         }
     },
     computed: {
-        ...mapState(['showForm'])
+        ...mapState(['showForm', 'currentToy'])
     }
 
 }
--- src/store/index.js
+++ src/store/index.js
@@ -7,14 +7,29 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     toys : [],
-    showForm: false
+    showForm: false,
+    currentToy:{
+      data:{
+        name:'',
+        code:'',
+        price: 0,
+        stock:0
+      },
+      id: null,
+    }
   },
   mutations: {
     SET_TOYS(state, data){state.toys = data},
     // Cuando queramos que se muestre o no el formulario
     DISPLAY_TOYFORM(state){state.showForm = true },
-    HIDE_TOYFORM(state){state.showForm = false}
+    HIDE_TOYFORM(state){state.showForm = false},
+    UPDATE_NAME(state, name) {state.currentToy.data.name = name},
+    UPDATE_PRICE(state, price) {state.currentToy.data.price = price},
+    UPDATE_CODE(state, code) {state.currentToy.data.code = code},
+    UPDATE_STOCK(state, stock) {state.currentToy.data.stock = stock},
   },
+
+
   actions:{ 
     setToys({commit}){
       axios.get('https://us-central1-ottoklaus-b9753.cloudfunctions.net/toys/toys')
@@ -22,13 +37,20 @@ export default new Vuex.Store({
         commit('SET_TOYS', response.data)
       })
     },
-    displayToyForm({commit}){
-      commit('DISPLAY_TOYFORM')
-    },
-    hideToyForm({commit}){
-      commit('HIDE_TOYFORM')
+    displayToyForm({commit}){commit('DISPLAY_TOYFORM')},
+    hideToyForm({commit}){commit('HIDE_TOYFORM')},
+    updateName({commit}, name){commit('UPDATE_NAME', name)},
+    updatePrice({commit}, price){commit('UPDATE_PRICE', price)},
+    updateCode({commit}, code){commit('UPDATE_CODE', code)},
+    updateStock({commit}, stock){commit('UPDATE_STOCK', stock)},
+    postToy({dispatch, state}){
+      axios.post('https://us-central1-ottoklaus-b9753.cloudfunctions.net/toys/toy', state.currentToy.data)
+      .then(()=> {
+        dispatch('setToys')
+      })
     }
   }
+  
 
  
 })
